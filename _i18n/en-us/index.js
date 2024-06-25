6d56b5f93f8b81f6080fe0f7d88979b0592d2f9e
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-06-25 16:23:55 -0500

    + added translations

--- _i18n/en-us/index.js
+++ _i18n/en-us/index.js
@@ -1,7 +1,28 @@
-import layout from 'modules/qplace/_i18n/en-us/layout'
-import sidebar from 'modules/qplace/_i18n/en-us/sidebar'
-
 export default {
-  layout,
-  sidebar
+  "iplaces": {
+    "cms": {
+      "sidebar": {
+        "adminGroup": "Places",
+        "adminPlaces": "Places List",
+        "adminPlacesCreate": "Create Place",
+        "adminPlacesUpdate": "Update Place",
+        "adminCategories": "Place Categories",
+        "adminSchedules": "Schedules"
+      },
+      "newPlace": "New Place",
+      "updatePlace": "Update Place",
+      "newCategory": "New Category",
+      "updateCategory": "Update Category",
+      "form": {
+        "fromDay": "From day",
+        "untilDay": "Until day",
+        "fromTime": "From time",
+        "untilTime": "Until time"
+      },
+      "documentation": {
+        "places": "With Places, you can publish on your site information about real places, with title, description images and a location within a map with address. This information is used to give information about your company headquarters.",
+        "categories": "It is possible to create categories, with a name, description and organize them in tree mode (parent categories and child categories) in order to categorize all your places."
+      }
+    }
+  }
 }
--- _i18n/es/index.js
+++ _i18n/es/index.js
@@ -1,7 +1,28 @@
-import layout from 'modules/qplace/_i18n/es/layout'
-import sidebar from 'modules/qplace/_i18n/es/sidebar'
-
 export default {
-  layout,
-  sidebar
+  "iplaces": {
+    "cms": {
+      "sidebar": {
+        "adminGroup": "Lugares",
+        "adminPlaces": "Lista de Lugares",
+        "adminPlacesCreate": "Crear Lugar",
+        "adminPlacesUpdate": "Actualizar Lugar",
+        "adminCategories": "Categorías de Lugares",
+        "adminSchedules": "Horarios"
+      },
+      "newPlace": "Nuevo lugar",
+      "updatePlace": "Actualizar Lugar",
+      "newCategory": "Nueva Categoría",
+      "updateCategory": "Actualizar Categoría",
+      "form": {
+        "fromDay": "Desde el día",
+        "untilDay": "Hasta el día",
+        "fromTime": "Desde la hora",
+        "untilTime": "Hasta la hora"
+      },
+      "documentation": {
+        "places": "Con los Lugares, puedes publicar en tu sitio información sobre lugares reales, con titulo, descripción imágenes y una ubicación dentro de un mapa con dirección. Está información es usada para dar información sobre sedes de tu empresa.",
+        "categories": "Es posible crear categories, con un nombre, descripción y organizarlas en modo de árbol (categorías padres y categorías hijas) con el fin de categorizar todos tus lugares."
+      }
+    }
+  }
 }
